6f058cf7368d01c9eed33970eac5886936c5ec19
Armando Reyna <areyna@sentinel.com>
2020-02-26 12:56:16 -0600

    fix validate service

--- ci/validate_service.sh
+++ ci/validate_service.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+
+logfile="/home/ec2-user/ci/poc01.log"
+
+(tail -f -n0 $logfile &) | grep -q "Started Poc01Application"
+
 STATUS=$(curl http://localhost:8082/actuator/health)
 
-if [ $STATUS -eq "{"status":"UP"}" ]
-then
+if [ $STATUS -eq "{"status":"UP"}" ]; then
   echo "Service is UP"
   exit 0
 else
