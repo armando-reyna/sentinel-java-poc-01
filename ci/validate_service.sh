870bb77646fe8772f8891ddc6fbf84414e3e9499
Armando Reyna <areyna@sentinel.com>
2020-03-10 15:13:12 -0500

    fix stop script

--- ci/validate_service.sh
+++ ci/validate_service.sh
@@ -2,7 +2,7 @@
 
 logfile="/home/ec2-user/ci/poc01.log"
 
-(tail -f -n0 $logfile &) | grep -q "Started Poc01Application"
+(tail -f -n0 $logfile &) | grep -q "Tomcat started on port"
 
 health=$(curl http://localhost:8082/actuator/health)
 
